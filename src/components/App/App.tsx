02b1773357f1e541340b37cb3cf45c7ed52dfe14
juliya.lysenko <juliya.deleh@gmail.com>
2025-07-19 01:20:32 +0300

    commit4

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,27 +1,27 @@
-import { useState, useEffect } from "react";
-import { useQuery, keepPreviousData } from "@tanstack/react-query";
+// import { useState, useEffect } from "react";
+// import { useQuery, keepPreviousData } from "@tanstack/react-query";
 
-import css from "./App.module.css";
-import ErrorMessage from "../ErrorMessage/ErrorMessage";
-import Loader from "../Loader/Loader";
-import Modal from "../Modal/Modal";
-import NoteForm from "../NoteForm/NoteForm";
-import NoteList from "../NoteList/NoteList";
-import SearchBox from "../SearchBox/SearchBox";
-import Pagination from "../Pagination/Pagination";
+// import css from "./App.module.css";
+// import ErrorMessage from "../ErrorMessage/ErrorMessage";
+// import Loader from "../Loader/Loader";
+// import Modal from "../Modal/Modal";
+// import NoteForm from "../NoteForm/NoteForm";
+// import NoteList from "../NoteList/NoteList";
+// import SearchBox from "../SearchBox/SearchBox";
+// import Pagination from "../Pagination/Pagination";
 
-export default function App() {
-  return (
-    <div className={css.app}>
-      <header className={css.toolbar}>
-        {<SearchBox />}
-        {<Pagination />}
-        {<button className={css.button}>Create note +</button>}
-      </header>
+// export default function App() {
+//   return (
+//     <div className={css.app}>
+//       <header className={css.toolbar}>
+//         {<SearchBox />}
+//         {<Pagination />}
+//         {<button className={css.button}>Create note +</button>}
+//       </header>
 
-      <NoteList />
-      <NoteForm />
-      <Modal />
-    </div>
-  );
-}
+//       <NoteList />
+//       <NoteForm />
+//       <Modal />
+//     </div>
+//   );
+// }
